78fc1285791fbc05c1056ae4983a639fe8743b90
FranMoraton <famoraton@gmail.com>
2022-03-13 20:03:01 +0100

    add fs read to string

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let query = &args[1];
+    let file = &args[2];
+
+    println!("{:?}", query);
+    println!("{:?}", file);
+
+    let contents = fs::read_to_string(file).expect("file not valid");
+    println!("{}", contents);
 }
