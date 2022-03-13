9721a65ad727af7bde1c0a232e6ec72438e04651
FranMoraton <famoraton@gmail.com>
2022-03-13 21:11:22 +0100

    handling not enough parameters

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("{}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(config.file).expect("file not valid");
-    println!("{}", contents);
+    run(config);
 }
-
 
 struct Config {
     haystack: String,
@@ -16,10 +17,19 @@ struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String])-> Config {
-        Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough parameters expect haystack and file");
+        }
+
+        Ok(Config {
             haystack: args[1].to_owned(),
             file: args[2].to_owned(),
-        }
+        })
     }
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file).expect("file not valid");
+    println!("{}", contents);
 }
