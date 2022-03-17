427db685e8d5068c86545f930bd5194d2fa3a1bf
FranMoraton <famoraton@gmail.com>
2022-03-17 17:51:56 +0100

    change errors to eprintln so it wont use standard output

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("application error: {}", err);
+        eprintln!("application error: {}", err);
         process::exit(1);
     });
 
     if let Err(err) = file_read_lib::run(config) {
-        println!("application error: {}", err);
+        eprintln!("application error: {}", err);
         process::exit(1);
     };
 }
